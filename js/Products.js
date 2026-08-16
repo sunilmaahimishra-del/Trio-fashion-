839a0f1c8ac2fe685163ea6d76378802aad00a1c
sunilmaahimishra-del <sunilmaahi.mishra@gmail.com>
2026-08-16 14:35:56 +0530

    Add products array to Products.js

--- js/Products.js
+++ js/Products.js
@@ -1,1 +1,17 @@
 
+const products = [
+  {
+    id: 1,
+    name: "Trio Fashion Product 1",
+    price: 999,
+    image: "IMG-20260816-WA0001.jpg"
+  },
+  {
+    id: 2,
+    name: "Trio Fashion Product 2",
+    price: 999,
+    image: "IMG-20260816-WA0002.jpg"
+  }
+];
+
+window.products = products;
