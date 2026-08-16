7268391a44a49f0ba85a7e055b4c4b04e25acf0d
sunilmaahimishra-del <sunilmaahi.mishra@gmail.com>
2026-08-16 14:46:36 +0530

    Add new products to Products.js
    Added multiple products to the products array and updated the existing product.

--- js/Products.js
+++ js/Products.js
@@ -1,4 +1,61 @@
+const products = [
+  {
+    id: 2,
+    name: "Trio Fashion Dress 2",
+    price: 999,
+    image: "IMG-20260816-WA0002.jpg"
+  },
+  {
+    id: 3,
+    name: "Trio Fashion Dress 3",
+    price: 999,
+    image: "IMG-20260816-WA0003.jpg"
+  },
+  {
+    id: 4,
+    name: "Trio Fashion Dress 4",
+    price: 999,
+    image: "IMG-20260816-WA0004.jpg"
+  },
+  {
+    id: 5,
+    name: "Trio Fashion Dress 5",
+    price: 999,
+    image: "IMG-20260816-WA0005.jpg"
+  },
+  {
+    id: 6,
+    name: "Trio Fashion Dress 6",
+    price: 999,
+    image: "IMG-20260816-WA0006.jpg"
+  },
+  {
+    id: 7,
+    name: "Trio Fashion Dress 7",
+    price: 999,
+    image: "IMG-20260816-WA0007.jpg"
+  },
+  {
+    id: 8,
+    name: "Trio Fashion Dress 8",
+    price: 999,
+    image: "IMG-20260816-WA0008.jpg"
+  },
+  {
+    id: 9,
+    name: "Trio Fashion Dress 9",
+    price: 999,
+    image: "IMG-20260816-WA0009.jpg"
+  },
+  {
+    id: 10,
+    name: "Trio Fashion Dress 10",
+    price: 999,
+    image: "IMG-20260816-WA0010.jpg"
+  }
+];
 
+window.products = products;
 const products = [
   {
     id: 1,
