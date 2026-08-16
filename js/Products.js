3884ea700d228260439455f1cadf1dc7218fe3c8
sunilmaahimishra-del <sunilmaahi.mishra@gmail.com>
2026-08-16 14:54:17 +0530

    Update Products.js

--- js/Products.js
+++ js/Products.js
@@ -57,18 +57,7 @@ const products = [
 
 window.products = products;
 const products = [
-  {
-    id: 1,
-    name: "Trio Fashion Product 1",
-    price: 999,
-    image: "IMG-20260816-WA0001.jpg"
-  },
-  {
-    id: 2,
-    name: "Trio Fashion Product 2",
-    price: 999,
-    image: "IMG-20260816-WA0002.jpg"
-  }
-];
+  
+  
 
 window.products = products;
